b62ab176d3ff943a2691b34c33fd500221d57d04
victor <vfranktor@gmail.com>
2023-05-25 14:58:25 +0100

    feat(*): set foundation for local llm

--- __init__.py
+++ __init__.py
@@ -1,61 +1,60 @@
-import openai as ai
+# import openai as ai
 from core.skills.common_query_skill import CommonQuerySkill, CQSMatchLevel
+from core.skills import FallbackSkill
+import gpt4all
 
-model = "gpt-3.5-turbo"
 
-
-class FallbackChatgpt(CommonQuerySkill):
+class FallbackGPT(CommonQuerySkill, FallbackSkill):
     def __init__(self):
         super().__init__()
         self.key = self.settings.get("key")
+        self.gpt = None
+        self.response = None
 
     def initialize(self):
+        # the slow answer is moved over to fallback
+        # self.register_fallback(self.handle_fallback, 90)
         pass
 
     def CQS_match_query_phrase(self, utt):
-        response = self.handle_fallback_ChatGPT(utt)
-        # self.log.info(response)
+        response = self.answer_question(utt)
         if response:
-            if "?" in response:
+            if response.endswith("?"):
                 return None
             return (utt, CQSMatchLevel.CATEGORY, response)
         return None
 
-    def handle_fallback_ChatGPT(self, message):
-        # prompt = self.build_prompt(message.data['utterance'])
+    def answer_question(self, message):
+        self.gptj = gpt4all.GPT4All("ggml-gpt4all-j-v1.3-groovy")
         prompt = self.build_prompt(message)
 
-        self.log.info(f'prompt: {prompt}')
-        # self.log.info(type(self.init_prompt))
+        self.log.info('prompt: {}'.format(prompt))
 
         try:
-            completion = self.chatgpt.create(model=model, messages=prompt,
-                                             max_tokens=100, temperature=0.2)
-            response = completion.choices[0].message["content"]
-            if not response or not response.strip("?") or not response.strip("_"):
-                return False
+            response = self.gptj.chat_completion(prompt)
+            # if not response or not response.strip("?") or not response.strip("_"):
+            #     return False
+            # unwanted_string = "As an AI language model,"
+            # response = response.replace(unwanted_string, "")
+            response = response['choices'][0]['message']['content']
             self.log.info(f'response: {response}')
-            unwanted_string = "As an AI language model,"
-            response = response.replace(unwanted_string, "")
+            self.response = response
             return response
         except Exception as e:
-            self.log.error(f'error in ChatGPT fallback request: {e}')
+            self.log.error('error in GPT fallback request: {}'.format(e))
             return None
+
+    # def handle_fallback(self, message):
+    #     if self.answer_question is not None:
+    #         self.speak(self.response,
+    #                    expect_response=True if "?" in self.response else False)
 
     def build_prompt(self, prompt):
         return [{"role": "user", "content": prompt}]
-
-    @property
-    def chatgpt(self):
-        # key = self.settings.get("key") or api_key
-        if not self.key:
-            raise ValueError("Openai key not set in settings.json")
-        ai.api_key = self.key
-        return ai.ChatCompletion
 
     def stop(self):
         pass
 
 
 def create_skill():
-    return FallbackChatgpt()
+    return FallbackGPT()
