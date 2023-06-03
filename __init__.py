21f1071fc6be9812936f941877feae1cadb1ca0e
victor <vfranktor@gmail.com>
2023-06-03 23:07:00 +0100

    feat(*): add conversation with llm

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
 import openai as ai
+import os
 
-from core import Skill
+
+from threading import Thread
+from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from core import Skill, intent_file_handler, Message
 from core.skills.common_query_skill import CommonQuerySkill, CQSMatchLevel
 
 model = "gpt-3.5-turbo"
@@ -19,13 +25,20 @@ class FallbackGPT(CommonQuerySkill, Skill):
         self.key = self.settings.get("key")
 
     def initialize(self):
-        pass
+        self.llm = OpenAI(temperature=0.2)
+        self.memory = ConversationBufferMemory()
+        os.environ["OPENAI_API_KEY"] = self.key
 
     def CQS_match_query_phrase(self, utt):
         response = self.handle_fallback_GPT(utt)
         if response:
             return (utt, CQSMatchLevel.CATEGORY, response)
         return None
+
+    @property
+    def chat_timeout(self):
+        """The chat_timeout in seconds."""
+        return self.settings.get("chat_timeout") or 300
 
     def handle_fallback_GPT(self, message):
         prompt = self.build_prompt(message)
@@ -46,7 +59,40 @@ class FallbackGPT(CommonQuerySkill, Skill):
             self.log.error('error in fallback request: {}'.format(e))
             return None
 
-    # def converse()
+    @intent_file_handler("start_conversation.intent")
+    def handle_converse_init(self, message):
+        initial_prompt = "What do you want to talk about?"
+        self.memory.chat_memory.add_ai_message(initial_prompt)
+        self.speak_dialog(initial_prompt, wait=True)
+        self.make_active()
+        self.bus.emit(Message("core.mic.listen"))
+
+    def _get_llm_response(self, query: str) -> str:
+        """
+        Get a response from an LLM that is primed/prompted with chat history
+        """
+        conversation = ConversationChain(llm=self.llm, verbose=True,
+                                         memory=self.memory)
+        return conversation.predict(input=query)
+
+    def converse(self, message=None):
+        # Track timeout and return false.
+        # if time() -
+        utterance = message.data.get('utterances', [""])[-1]
+        if self.voc_match(utterance, "stop"):
+            self.speak_dialog("ending conversation")
+            return False
+        self.log.info("utterance: {}".format(utterance))
+        Thread(target=self._threaded_converse, args=([utterance]), daemon=True).start()
+        return True
+
+    def _threaded_converse(self, utterance):
+        try:
+            response = self._get_llm_response(utterance)
+            self.speak(response, expect_response=True if response.endswith("?") or "?"
+                       in response else False)
+        except Exception as e:
+            self.log.exception(e)
 
     def build_prompt(self, prompt):
         return [{"role": "user", "content": f"""You are a personal assistant. Answer
