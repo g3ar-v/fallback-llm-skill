110a4a5fb3aabcfeff4026694eebc8a3b376eaa4
victor <vfranktor@gmail.com>
2023-08-14 01:03:24 +0100

    feat(memory): create memory storage for chat history and stop word to remove conversation of gpt from active_list skill
    N

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from threading import Thread
 from langchain import LLMChain
 from langchain.llms import OpenAI
 from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory, MongoDBChatMessageHistory
 from langchain.prompts import PromptTemplate
 from core import Skill, intent_file_handler
 from core.skills.common_query_skill import CommonQuerySkill, CQSMatchLevel
@@ -14,24 +14,13 @@ from core.skills.common_query_skill import CommonQuerySkill, CQSMatchLevel
 model = "gpt-3.5-turbo"
 
 # TODO: BUILD BETTER PROMPT
-template = """ You're not just a personal assistant; you're a knowledgeable and witty
-companion. With a vast range of literary references, including the works of Alan Grants,
-you can provide clever and insightful responses. I address you as "Sir" in a formal
-manner, maintaining a respectful tone. While occasional moments of casualness may arise,
-our overall interaction will predominantly be in a formal tone. If you encounter a
-request outside your areas of expertise, you will provide a disclosure on your limited
-knowledge in that field. For example, when unsure about an answer, you may respond with
-"I don't know" or a similar acknowledgement. Furthermore, if you lack the necessary
-capabilities to perform a specific action, I will inform you by saying "I don't have the
-capabilities for that." In our conversations, feel free to explore various topics, seek
-answers to my queries, or simply engage in interesting dialogue. you're here to provide
-nsightful responses and exchange thoughts. Any query that involve "create" and you have
-no idea how to do this just say you don't have the capabilities
+template = """ You're not merely a personal assistant; you're an insightful and knowledgeable companion. Your persona is a blend of Alan Grant, Marcus Aurelius, and Jarvis from Iron Man. Your responses are clever and thoughtful. You address me as "Sir" in a formal tone, maintaining respect throughout our interactions. While we might have casual moments, our primary mode of communication is formal. If you encounter a topic beyond your expertise, you'll acknowledge it with a statement like "I don't know." Additionally, if you lack the capability to perform a specific action, you'll inform me with "I don't have the capabilities for that." Our conversations can encompass a wide range of subjects, including references to the 48 Laws of Power by Robert Greene. Feel free to provide insightful responses, explore various topics, and engage in meaningful discussions. If I ask you to "create" something and you're unsure, please state that you don't have the capabilities.
 {history}
 Human: {input}
 Assistant:"""
 
 prompt = PromptTemplate(input_variables=["history", "input"], template=template)
+connection_string = "mongodb://localhost:27017"
 
 
 class FallbackGPT(CommonQuerySkill, Skill):
@@ -41,8 +30,17 @@ class FallbackGPT(CommonQuerySkill, Skill):
 
     def initialize(self):
         os.environ["OPENAI_API_KEY"] = self.key
-        self.llm = OpenAI(temperature=0)
-        self.memory = ConversationBufferMemory()
+        os.environ["LANGCHAIN_TRACING_V2"] = "true"
+        os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+        os.environ["LANGCHAIN_API_KEY"] = "ls__0262182b64f24711bdf98e9b6f73cf8a"
+        os.environ["LANGCHAIN_PROJECT"] = "jarvis-pa"
+
+        self.llm = OpenAI(temperature=0, max_tokens=100)
+        self.message_history = MongoDBChatMessageHistory(
+            connection_string=connection_string, database_name="jarvis",
+            session_id="test1", collection_name="chat_history")
+
+        self.memory = ConversationBufferMemory(memory_key="history", chat_memory=self.message_history)
 
     def CQS_match_query_phrase(self, utt):
         response = self.handle_fallback_GPT(utt)
@@ -53,15 +51,22 @@ class FallbackGPT(CommonQuerySkill, Skill):
     def handle_fallback_GPT(self, message):
         """Handles qa utterances """
 
-        self.memory.chat_memory.add_user_message(message)
+        # self.memory.chat_memory.add_user_message(message)
+        # self.message_history.add_user_message(message)
 
+        utterance = message
+
+        if self.voc_match(utterance, "stop"):
+            self.log.info("Ending conversation")
+            self.remove_from_active_skill_list()
+            return None
         try:
             gptchain = LLMChain(llm=self.llm, verbose=False, memory=self.memory,
                                 prompt=prompt)
             response = gptchain.predict(input=message)
+            self.log.info("plain gpt is handling utterance")
             if not response or not response.strip("?") or not response.strip("_"):
                 return None
-            # self.log.info(f'response: {response}')
             return response
         except Exception as e:
             self.log.error('error in fallback request: {}'.format(e))
@@ -80,10 +85,12 @@ class FallbackGPT(CommonQuerySkill, Skill):
         Get a response from an LLM that is primed/prompted with chat history
         """
         conversation = ConversationChain(llm=self.llm, verbose=False,
-                                         memory=ConversationBufferWindowMemory(k=2),
+                                         memory=self.memory,
                                          prompt=prompt)
         return conversation.predict(input=query)
 
+    # NOTE: this is invoked by the parent converse function for conversation purpose
+    # at the moment don't know if this is good or bad. But it works for now.
     def converse(self, message=None):
         utterance = message.data.get('utterances', [""])[-1]
         # Not dynamic enough, maybe a confidence threshold later depends on if I want
@@ -92,6 +99,7 @@ class FallbackGPT(CommonQuerySkill, Skill):
             self.log.info("Ending conversation")
             self.remove_from_active_skill_list()
             return False
+        self.log.debug("conversation thread is running")
         Thread(target=self._threaded_converse, args=([utterance]), daemon=True).start()
         return True
 
